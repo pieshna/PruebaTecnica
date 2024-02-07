348c5eab6e7229c19d432a9622d272bd4c8ecfe9
Pieshna <cabreraj525@gmail.com>
2024-02-06 23:20:53 -0600

    migración para crear la tabla de reportes

---
+++ ProyectoLaravel/database/migrations/2024_02_07_051800_create_reportes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reportes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_reporte');
+            $table->enum('estado', ['ACT', 'REC', 'COM'])->default('ACT');
+            $table->foreignId('userId')->references('id')->on('usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reportes');
+    }
+};
